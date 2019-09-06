cb51ca314d00ef57cdf88e0f06b6b3b542d7fd10
Konstantin Khomutov <khomutov-k@mail.ru>
2019-09-06 20:25:36 +0300

    Task 3 is ready.

---
+++ HelloWorld/src/Task3.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+import java.util.Random;
+import java.util.Scanner;
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0;i<7;i++) {
+			int rand = new Random().nextInt(50) + 1;
+			System.out.print(rand+" ");
+	}
+	}
+}
