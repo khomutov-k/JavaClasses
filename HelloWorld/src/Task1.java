2d4a8273957af07332b69d6e97af98fd3f50ed38
Konstantin Khomutov <khomutov-k@mail.ru>
2019-09-06 20:13:47 +0300

    First exercise is done.

---
+++ HelloWorld/src/Task1.java
@@ -0,0 +1,10 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "Konstantin";
+		System.out.println("Hello, " + myName+". How are you?");
+	}
+
+}
