0cba65ee6f37c91948f54a634bd6dec13839bb93
Konstantin Khomutov <khomutov-k@mail.ru>
2019-09-11 19:32:06 +0300

    Name changes.

--- HelloWorld/Week2/W2task1.java
+++ HelloWorld/Week2/W2task1.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 import java.util.Random;
 import java.util.Scanner;
-public class Task1w2 {
+public class W2task1 {
 	 public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input a number:");
